70ad85a67b2b7abeb5cefed0f5e08d8802b4a745
Murat Aslan <murataslanx33@gmail.com>
2017-12-16 22:36:09 +0300

    Update text-to-base64.py

--- text-to-base64.py
+++ text-to-base64.py
@@ -1,5 +1,4 @@
 import base64
 string = input("Some text here : ")
 data = base64.b64encode(string.encode())
-print("Encoded : ", data)
-print("Decoded : ", base64.b64decode(data))
+print("Encoded : ", data, "\nDecoded : ", base64.b64decode(data))
